d9bab067c9d93f9ce2f0f15b3408dd1057707318
Carlos Bolaños <cabscarlos26@hotmail.com>
2023-12-12 10:14:14 -0500

    solucionado el redireccionamiento del localhost

--- src/routers/home.router.js
+++ src/routers/home.router.js
@@ -3,15 +3,13 @@ import { Router }  from 'express';
 
 const router = Router();
 
-// const EMAIL_USER = 'a@b.c'
-// const PASSWORD_USER = 'qwerty'
+router.get('/', (req, res) => {     
+   if(!req.session.user){
+     return res.redirect('/login')
+   }
+    res.redirect('/dbproducts');
+ });
 
-// const isAuth = (req, res, next) =>{
-//   if (!req.session.isAuth){
-//     return res.status(401).send('No tienes permiso de estar aquí 🫣')
-//   }
-//   next()
-// };
 
 router.get('/profile', (req, res) => {     
   if(!req.session.user){
@@ -27,38 +25,6 @@ router.get('/login', (req, res) => {
 router.get('/register', (req, res) => {     
    res.render('register', {title: 'Bienvenido', });
 });
-
-// http://localhost:8080/login?email=a@b.c&password=qwerty
-
-// router.get('/login', (req, res) =>{
-//   const {email, password} = req.query;
-//     if(email != EMAIL_USER || password != PASSWORD_USER){
-//       return res.status(401).send ('Usuario o contraseña son invalidos 😔')
-//     }
-//     req.session.email = email;
-//     req.session.isAdmin = true;
-//     req.session.isAuth = true;
-//     res.status(200).send('ok')
-// });
-
-// router.get('/profile', isAuth,  (req, res) => {
-//   res.json({
-//     email: req.session.email,
-//     isAdmin: req.session.isAdmin,
-
-//   })
-// }) 
-
-// router.get('/logout', (req, res) => {
-//   req.session.destroy((error) => {
-//     if (error){
-//       res.status(500).json({ message: 'Ha ocurrido un error surante el logout'})
-//     }else{
-//       res.status(200).json({ message: 'Se ha cerrado sesion correctamente'})
-//     };
-//   });
-// });
-
 
 
 
